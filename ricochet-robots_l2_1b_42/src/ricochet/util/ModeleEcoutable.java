1c2a9b87012c73f9107dd51aa4ee8fde2bf4c44a
Dark-Brandy <53907600+Dark-Brandy@users.noreply.github.com>
2022-05-18 15:08:57 +0200

    Final ricochet robot

---
+++ ricochet-robots_l2_1b_42/src/ricochet/util/ModeleEcoutable.java
@@ -0,0 +1,11 @@
+package ricochet.util;
+
+/**
+ * Interface implémentée par les modèles. L'ajout et le retrait d'écouteur
+ * doit être redéfini.
+ */
+public interface ModeleEcoutable {
+
+	public void ajoutEcouteur(EcouteurModele e);
+	public void retraitEcouteur(EcouteurModele e);
+}
